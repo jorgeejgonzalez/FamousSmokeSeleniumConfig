d814c8f53b1d41ab783e81cf3ccf4501e3502f97
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-01-23 16:58:58 -0300

    initialized the configuration

---
+++ start-hub.sh
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone-2.49.0.jar -role hub -hubConfig hubConfig.json
---
+++ start-node.bat
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone-2.49.0.jar -role webdriver -nodeConfig nodeConfig-windows.json
