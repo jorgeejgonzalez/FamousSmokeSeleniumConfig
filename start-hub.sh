87e63623da1bbe22463392add21d11c84bb56a2f
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-03-28 22:14:02 -0300

    updated windows config and added logging to selenium hub

--- start-hub.sh
+++ start-hub.sh
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-2.53.0.jar -role hub -hubConfig hubConfig.json
+java -jar selenium-server-standalone-2.53.0.jar -role hub -hubConfig hubConfig.json &> /var/log/selenium-hub.log
