e155676039f7bee81c84c2d76c66394b79daba7a
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-03-28 21:37:07 -0300

    updated the scripts to the new selenium version

--- start-node.bat
+++ start-node.bat
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-2.49.0.jar -role webdriver -nodeConfig nodeConfig-windows.json
+java -jar selenium-server-standalone-2.53.0.jar -role webdriver -nodeConfig nodeConfig-windows.json
